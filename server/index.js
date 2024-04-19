7e923d418aa5c7ed893763d4517dcc7e37f70020
Sahas Timilsina <tsahas2059@gmail.com>
2024-04-19 13:27:49 +0545

    refactor: code divided to modules

--- server/index.js
+++ server/index.js
@@ -1,35 +1,17 @@
 const express = require("express");
-const movies = require("./movies.json");
 const cors = require("cors");
-const { prisma } = require("./db");
 
 const app = express();
 
+app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
   return res.send("Hello, World");
 });
 
-app.get("/movies/list", async (req, res) => {
-  const offset = parseInt(req.query.offset);
-  const count = await prisma.movie.count();
-  const movies = await prisma.movie.findMany({
-    take: 12,
-    skip: offset,
-  });
-  return res.json({ movies, count });
-});
-
-app.get("/movie/:id", async (req, res) => {
-  const id = req.params.id;
-  const movie = await prisma.movie.findUnique({
-    where: {
-      id: parseInt(id),
-    },
-  });
-  return res.send(movie);
-});
+app.use("", require("./routes/movies"));
+app.use("/auth", require("./routes/auth"));
 
 app.listen(8080, () => {
   console.log("Listening on port 8080");
