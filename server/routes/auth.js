2117ae9727c77c3d9917ada486fe73e1d75cf4ae
Sahas Timilsina <tsahas2059@gmail.com>
2024-04-19 13:28:27 +0545

    feat: added validation for signup

---
+++ server/routes/auth.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const { check, validationResult } = require("express-validator");
+
+router.post("/signup", [
+  check("email", "Please input a valid email").isEmail(),
+  check("password", "Please input a password with a minimum length of 6")
+    .isLength({ min: 6 }),
+  check("username", "Please input a username with a minimum length of 6")
+    .isLength({ min: 6 }),
+], async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+    });
+  }
+
+  res.send("VAlID");
+
+  const { email, password, username } = req.body;
+});
+
+module.exports = router;
