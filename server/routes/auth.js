2719b1781f1795d9d704cdaddc05508e0ea14923
Sahas Timilsina <tsahas2059@gmail.com>
2024-04-20 10:46:01 +0545

    feat: Added authentication endpoints

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,24 +1,142 @@
 const router = require("express").Router();
 const { check, validationResult } = require("express-validator");
+const { prisma } = require("../db");
+const bcrypt = require("bcrypt");
+const JWT = require("jsonwebtoken");
 
-router.post("/signup", [
-  check("email", "Please input a valid email").isEmail(),
-  check("password", "Please input a password with a minimum length of 6")
-    .isLength({ min: 6 }),
-  check("username", "Please input a username with a minimum length of 6")
-    .isLength({ min: 6 }),
-], async (req, res) => {
-  const errors = validationResult(req);
+router.post(
+  "/signup",
+  [
+    check("email", "Please input a valid email").isEmail(),
+    check(
+      "password",
+      "Please input a password with a minimum length of 6"
+    ).isLength({ min: 6 }),
+    check(
+      "username",
+      "Please input a username with a minimum length of 6"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    const { email, password, username } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (user) {
+      return res.status(400).json({
+        errors: [{ msg: "This user already exists" }],
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        username,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+      },
+    });
+
+    const token = await JWT.sign(newUser, process.env.JSON_WEB_TOKEN_SECRET, {
+      expiresIn: 3300000,
+    });
+
+    return res.json({
+      user: newUser,
+      token,
+    });
+  }
+);
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (!user) {
     return res.status(400).json({
-      errors: errors.array(),
+      errors: [{ msg: "Invalid credentials" }],
     });
   }
 
-  res.send("VAlID");
+  const isMatch = await bcrypt.compare(password, user.password);
 
-  const { email, password, username } = req.body;
+  if (!isMatch) {
+    return res.status(400).json({
+      errors: [{ msg: "Invalid credentials" }],
+    });
+  }
+
+  const token = await JWT.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    process.env.JSON_WEB_TOKEN_SECRET,
+    {
+      expiresIn: 3300000,
+    }
+  );
+
+  const userPayload = {
+    id: user.id,
+    email: user.email,
+    username: user.username,
+  };
+
+  return res.json({
+    user: userPayload,
+    token,
+  });
+});
+
+router.get("/me", async (req, res) => {
+  const bearerToken = req.headers.authorization;
+  if (!bearerToken) return res.send(null);
+  const jwt = bearerToken.split("Bearer ")[1];
+  if (!jwt) return res.send(null);
+
+  let payload;
+
+  try {
+    payload = await JWT.verify(jwt, process.env.JSON_WEB_TOKEN_SECRET);
+  } catch (error) {
+    return res.send(null);
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: payload.email,
+    },
+    select: {
+      id: true,
+      username: true,
+      email: true,
+    },
+  });
+
+  return res.json(user);
 });
 
 module.exports = router;
