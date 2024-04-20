22108b44e3d87375a108fe2d9b5ac5bc9e9133da
Sahas Timilsina <tsahas2059@gmail.com>
2024-04-20 21:28:25 +0545

    Added Validation

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,7 +1,46 @@
+import { createContext, useState } from "react";
 import Input from "../components/Input";
 import NavBar from "../components/NavBar";
+import {
+  FieldErrors,
+  SubmitHandler,
+  useForm,
+  UseFormRegister,
+} from "react-hook-form";
+
+export type Inputs = {
+  email: string;
+  password: string;
+  name: string;
+};
+
+type AuthFormContextType = {
+  register: UseFormRegister<Inputs> | null;
+  errors: FieldErrors<Inputs>;
+};
+
+enum Variant {
+  SIGN_UP,
+  LOG_IN,
+}
+
+export const AuthFormContext = createContext<AuthFormContextType>({
+  register: null,
+  errors: {},
+});
 
 export default function LoginPage() {
+  const { register, handleSubmit, formState: { errors }, getValues } = useForm<
+    Inputs
+  >();
+  const [variant, setVariant] = useState(Variant.LOG_IN);
+
+  console.log(errors);
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="relative bg-black h-screen w-screen bg-opacity-50">
       <NavBar />
@@ -10,25 +49,72 @@ export default function LoginPage() {
           <h2 className="text-white text-4xl mb-8 font-semibold">
           </h2>
 
-          <form className="flex flex-col gap-4">
-            <Input />
-            <Input />
-            <Input />
-            <input
-              type="submit"
-              className="bg-red-400 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700"
-            />
-          </form>
-          <p className="text-neutral-500 mt-12">
-            <span className="text-white ml-1 hover:underline cursor-pointer">
-              First time using Netflix?
-            </span>
-          </p>
-          <p className="text-neutral-500 mt-12">
-            <span className="text-white ml-1 hover:underline cursor-pointer">
-              Already have an account?
-            </span>
-          </p>
+          <AuthFormContext.Provider value={{ register, errors }}>
+            <form
+              className="flex flex-col gap-4"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              {variant === Variant.SIGN_UP && (
+                <Input id="username" type="text" label="Username" name="name" />
+              )}
+              <Input
+                id="email"
+                type="email"
+                label="Email Address"
+                name="email"
+              />
+              <Input
+                id="password"
+                type="password"
+                label="Password"
+                name="password"
+                validate={variant === Variant.SIGN_UP
+                  ? () => {
+                    const password = getValues("password");
+                    if (password.length < 8) {
+                      return "Password must be greater than 8 characters";
+                    }
+                    if (!/[A-Z]/.test(password)) {
+                      return "Password must have at least one uppercase and lowercase letter";
+                    }
+                    if (!/[a-z]/.test(password)) {
+                      return "Password must have at least one uppercase and lowecase letter";
+                    }
+                    if (!/\d/.test(password)) {
+                      return "Password must have a number";
+                    }
+                    return true;
+                  }
+                  : undefined}
+              />
+              <input
+                type="submit"
+                className="bg-red-400 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700"
+              />
+            </form>
+          </AuthFormContext.Provider>
+
+          {variant === Variant.LOG_IN
+            ? (
+              <p
+                className="text-neutral-500 mt-12"
+                onClick={() => setVariant(Variant.SIGN_UP)}
+              >
+                <span className="text-white ml-1 hover:underline cursor-pointer">
+                  First time using Netflix?
+                </span>
+              </p>
+            )
+            : (
+              <p
+                className="text-neutral-500 mt-12"
+                onClick={() => setVariant(Variant.LOG_IN)}
+              >
+                <span className="text-white ml-1 hover:underline cursor-pointer">
+                  Already have an account?
+                </span>
+              </p>
+            )}
         </div>
       </div>
     </div>
